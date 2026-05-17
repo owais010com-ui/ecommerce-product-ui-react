9f419aac206e7dbf4eabdc396167133a61089a55
owaisahmed123 <owais010.com@gmail.com>
2026-05-17 21:30:18 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -9,39 +9,61 @@ import "./App.css"
 export const App = () => {
 
   const [productData, setproductData] = useState([]);
+  const [productItems, setProductItems] = useState([]);
+  const [selectItems, setSelectItems] = useState("");
 
 
   const getData = async () => {
+    // https://dummyjson.com/products/category/smartphones
     try {
-      const apiData = await axios.get("https://dummyjson.com/products");
+      let url = "https://dummyjson.com/products"
+      if (selectItems) {
+        url = `https://dummyjson.com/products/category/${selectItems}`
+      }
+      const apiData = await axios.get(url);
       setproductData(apiData.data.products);
-
     } catch (error) {
-      console.log(error)
+      console.log("error", error);
     }
 
   }
 
+  const apiProduct = async () => {
+    try {
+      const itemList = await axios.get("https://dummyjson.com/products/category-list")
+      setProductItems(itemList.data);
+    } catch (error) {
+      console.log("error", error)
+    }
+
+  };
+
   useEffect(() => {
     getData();
-  }, [])
+  }, [selectItems]);
+
+  useEffect(() => {
+    apiProduct();
+  }, []);
 
 
   return (
     <>
-      <Header />
+      <Header
+        selectItems={selectItems}
+        setSelectItems={setSelectItems}
+        productItems={productItems} />
       <div className='showData'>
         {productData.map((apiProducts) => {
-          console.log(apiProducts);
           return (
             <ApiCard key={apiProducts.id} product={apiProducts} />
           )
 
         })}
 
-      </div>x 
+      </div>
     </>
-  )
+  );
 }
 
 export default App;
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
-const Header = () => {
+const Header = ({ selectItems, setSelectItems, productItems }) => {
+
     return (
         <header className='header'>
             <h1>Product.Web</h1>
 
-            <div className='searchBox'>
-                <input
-                    type="text"
-                    placeholder="Search products..."
-                />
-            </div>
+            <select className='selectBox' value={selectItems}
+                onChange={(events) => {
+                    setSelectItems(events.target.value)
+                }}>
+                <option value="">ALL</option>
+                {productItems?.map((eachItems) => {
+                    return (
+                        <option key={eachItems} value={eachItems}>
+                            {eachItems}
+                        </option>
+                    );
+                })}
+            </select>
+
         </header>
     )
 }
